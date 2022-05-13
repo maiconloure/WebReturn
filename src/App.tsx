f51254cbc4d69073f9a20aa79af3838cf59ec10f
Maicon Lourenco <maiconloure@gmail.com>
2022-05-12 23:28:39 -0300

    added favicon, new home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import { Widget } from "./components/Widget"
 
 export function App() {
-  return <Widget />
+  return (
+    <div>
+      <div className="bg-zinc-800">
+        <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between flex-wrap">
+            <div className="h-10 w-40 bg-zinc-600 rounded-md">
+            </div>
+            <div className="h-3 w-20 bg-zinc-600 rounded-lg">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Widget />
+    </div>
+  )
 }
